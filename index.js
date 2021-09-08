2055ea5b969c28ec648adc676ce8d41e5a861609
Abhinav T K <63438668+abhinavtk@users.noreply.github.com>
2021-09-08 22:17:02 +0530

    Update: index

---
+++ index.js
@@ -0,0 +1,59 @@
+const billAmount = document.querySelector("#bill-amount");
+const cashGiven = document.querySelector("#cash-given");
+const nextButton = document.querySelector("#next-button");
+const checkButton = document.querySelector("#check-button");
+const message1 = document.querySelector("#error-message1");
+const message2 = document.querySelector("#error-message2");
+const next = document.querySelector("#next");
+const check = document.querySelector("#check");
+const noOfNotes = document.querySelectorAll(".no-of-notes");
+
+const availableNotes = [2000, 500, 100, 20, 10, 5, 1];
+next.style.display = "none";
+message1.style.display = "none";
+check.style.display = "none";
+//checkButton.addEventListener("click", showCheck());
+
+function calculateChange(amount){
+    for (let i=0; i<availableNotes.length; i++ ){
+        const numberOfNotes = Math.trunc(amount/availableNotes[i]);
+        amount %= availableNotes[i];
+        noOfNotes[i].innerText = numberOfNotes;
+    }
+} 
+
+function showMessage(msg,msgtxt){
+    msg.style.display = "block";
+    msg.innerText = msgtxt;
+}
+function showNext(){
+    if (billAmount.value > 0){
+        message1.style.display = "none";
+        next.style.display = "block";
+        check.style.display = "none";
+        nextButton.style.display = "none";
+    }
+    /* else if(typeof billAmount.value === 'string') {
+        message1.style.display = "block";
+        message1.innerText = "Invalid Bill Amount - Enter a positive number";
+    } */
+    else{
+        showMessage(message1,"Invalid Bill Amount");
+    }
+    
+}
+function showCheck(){
+    if (cashGiven.value >= billAmount.value){
+        message2.style.display = "none";
+        check.style.display = "block";
+        validateCashAndBillAmount();
+    }
+    else{
+        check.style.display = "none";
+        showMessage(message2,"The cash provided should atleast be equal to the bill amount.");
+    }
+}
+function validateCashAndBillAmount(){
+        const changeCash = cashGiven.value - billAmount.value;
+        calculateChange(changeCash);
+}
